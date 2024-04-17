ead4d280dd44144c96f9ce25815e460bf61b931c
lucast33 <167279581+lucast33@users.noreply.github.com>
2024-04-17 19:41:03 -0400

    Update attractions.py

--- flask-app/src/attractions/attractions.py
+++ flask-app/src/attractions/attractions.py
@@ -4,9 +4,9 @@ from src import db# Attractions Blueprint
 
 attractions = Blueprint('attractions', __name__)
 
-# Gets all rentals
+# Gets all attractions
 @attractions.route('/attractions', methods=['GET'])
-def get_rentals():
+def get_attractions():
     cursor = db.get_db().cursor()
     cursor.execute('select * from attractions')
     row_headers = [x[0] for x in cursor.description]
@@ -19,3 +19,32 @@ def get_rentals():
     the_response.mimetype = 'application/json'
     return the_response
 
+# Get attraction details for specific attraction location
+@attractions.route('/attractions/<location>', methods=['GET'])
+def get_attractionLocation(attractionLocation):
+    cursor = db.get_db().cursor()
+    cursor.execute('select * from attractions where attractionLocation = {0}'.format(attractionLocation))
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
+
+# Get attraction details for specific attraction category
+@attractions.route('/attractions/<category>', methods=['GET'])
+def get_amenity(amenityID):
+    cursor = db.get_db().cursor()
+    cursor.execute('select * from amenities where amenityID = {0}'.format(amenityID))
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
