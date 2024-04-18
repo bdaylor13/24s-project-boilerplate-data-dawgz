0eb6bfa027d30d5b67ab04d396d672d0560e88f1
bdaylor13 <166028864+bdaylor13@users.noreply.github.com>
2024-04-17 22:31:22 -0400

    Update attractions.py

--- flask-app/src/attractions/attractions.py
+++ flask-app/src/attractions/attractions.py
@@ -1,6 +1,7 @@
+# Attractions Blueprint
 from flask import Blueprint, request, jsonify, make_response, current_app
 import json
-from src import db# Attractions Blueprint
+from src import db
 
 attractions = Blueprint('attractions', __name__)
 
@@ -9,21 +10,6 @@ attractions = Blueprint('attractions', __name__)
 def get_attractions():
     cursor = db.get_db().cursor()
     cursor.execute('select * from attractions')
-    row_headers = [x[0] for x in cursor.description]
-    json_data = []
-    theData = cursor.fetchall()
-    for row in theData:
-        json_data.append(dict(zip(row_headers, row)))
-    the_response = make_response(jsonify(json_data))
-    the_response.status_code = 200
-    the_response.mimetype = 'application/json'
-    return the_response
-
-# Get attraction details for specific attraction location
-@attractions.route('/attractions/<location>', methods=['GET'])
-def get_attractionLocation(location):
-    cursor = db.get_db().cursor()
-    cursor.execute('select * from attractions where location = {0}'.format(location))
     row_headers = [x[0] for x in cursor.description]
     json_data = []
     theData = cursor.fetchall()
@@ -64,6 +50,7 @@ def add_new_attraction():
     db.get_db().commit()
     
     return 'Success!'
+    
 # update information about an attraction
 @attractions.route('/attractions', methods=['PUT'])
 def update_attraction():
@@ -109,7 +96,7 @@ def get_attractionLocation(location):
 
 # delete information about an attraction based on location
 
-@attractions.route('/attractions/,location.', methods=['DELETE'])
+@attractions.route('/attractions/<location>', methods=['DELETE'])
 def delete_attraction(location):
     
     # collecting data from the request object 
